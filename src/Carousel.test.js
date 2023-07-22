079bc9fe6fa1103b4ecb0285000b72d96d9358f7
Oscar Herrera <oscarhj1993@gmail.com>
2023-07-21 19:53:39 -0700

    excersise

---
+++ src/Carousel.test.js
@@ -0,0 +1,54 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import TEST_IMAGES from "./_testCommon.js";
+
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+it("Works when you click on the left arrow", function(){
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+})
+
+///Smoke test
+test ('It renders without crashing', () => {
+  render (<Carousel/>)
+});
+
+/// Snapshot test
+it("matches snapshot", function(){
+  const {asFragment} = render(<Carousel/>);
+  expect(asFragment()).toMatchSnapshot();
+});
